96408cd3f7ef339506c7bf67c63312d2220cb487
hhnavid <hhnavid@gmail.com>
2025-09-28 03:12:58 -0700

    dqn tested on acrobotv1

--- src/algos/dqn/dqn.py
+++ src/algos/dqn/dqn.py
@@ -1,17 +1,19 @@
 import sys
 sys.path.append('I:/projs/game-ai/src')
 
+import matplotlib.pyplot as plt
 import time
 import os
 import torch
 import torch.nn as nn
 from torch import Tensor
+from torch.nn import functional as F
 import numpy as np
 import random
 from datetime import datetime
 from common.my_replay_buffer import ReplayBuffer
 from common.utils import LinearSchedule
-from common.net_param_manip import init_xavier_weights
+from common.net_param_manip import init_xavier_weights, soft_update
 from common.running_mean_std import *
 from common.save_dict2csv import CSVLogger
 
@@ -36,6 +38,9 @@ class QNetwork(nn.Module):
             layer_items.append(('Linear{}'.format(i), torch.nn.Linear(fan_ins[i], fan_ins[i + 1])))
             
         [self.layers.add_module(name, layer) for name, layer in layer_items]
+        
+        # layer norm is disabled for now
+        self.layer_norm = False
 
         # Init network weights
         self.apply(init_xavier_weights)                            
@@ -67,7 +72,7 @@ class DQN:
                  obs_dim, obs_dtype, 
                  action_dim, action_dtype, is_action_discrete,
                  hidden_layers=[64, 63], activation_=nn.ReLU,
-                 normalize_obs=True, gamma=0.95, tau=1.0, lr=1e-4, bs=32,
+                 normalize_obs=True, gamma=0.94, tau=0.93, lr=1e-4, bs=32,
                  exploration_fraction=0.1, exploration_initial_eps=1.0,
                  exploration_final_eps=0.05, replay_buff_size=10000,
                  explore_render=False, eval_render=False,
@@ -127,7 +132,7 @@ class DQN:
         self.exploration_final_eps = exploration_final_eps
         self.exploration_fraction = exploration_fraction  
         self.current_progress_remaining = 1.0      
-        self.exploration_rate = 1.0
+        self.exploration_rate = self.exploration_initial_eps
         
         self.exploration_schedule = LinearSchedule(
             self.exploration_initial_eps,
@@ -141,9 +146,9 @@ class DQN:
         fan_ins = [self.obs_dim] + hidden_layers + [self.action_dim]
         self.q_network = QNetwork(fan_ins, activation_, lr).to(self.device)
         
-        self.target_network = QNetwork(fan_ins, activation_, lr).to(self.device)
-        self.target_network.load_state_dict(self.q_network.state_dict())
-        self.target_network.eval()
+        self.target_q_network = QNetwork(fan_ins, activation_, lr).to(self.device)
+        self.target_q_network.load_state_dict(self.q_network.state_dict())
+        self.target_q_network.eval()
 
         self.optimizer = torch.optim.Adam(self.q_network.parameters(), lr=lr)
         if is_action_discrete:
@@ -157,12 +162,14 @@ class DQN:
                                           action_shape=action_shape_, 
                                           action_dtype=action_dtype)            
         self.gamma = gamma
+        self.tau = tau
         self.batch_size = bs                                                
         
         # Observation normalization
         self.normalize_obs = normalize_obs
         if self.normalize_obs:
             self.obs_rms = RunningMeanStdMPI(shape=(self.obs_dim,))
+            self.obs_rms_history = {'mean': [], 'std': []}
         else:
             self.obs_rms = None
         self.normalize_reward = False
@@ -176,7 +183,7 @@ class DQN:
                                 
     
     def reset(self):
-        pass
+        pass    
     
         
     def init_train_variables(self):                
@@ -191,19 +198,25 @@ class DQN:
         # Each item of this list contains sum of immediate rewards for 1 explorative episode        
         self.epoch_episode_rewards = []  
         
+        # Each item of this list contains sum of immediate rewards for 1 evaluation episode                        
+        self.eval_episode_rewards = []  
+        
+        # loss values during the training process
+        self.q_losses = []
+        
         # Number of explorative steps performed in 1 episode. When episode is done, it's saved in
         #  `epoch_self.episode_steps` list. Next it is reset to 0.
         self.episode_step = 0        
         
-        combined_stats = {'rollout/return': 0.,
-                          'eval/return': 0.,
-                          'total/epochs': 0,
-                          'total/episodes': 0,
-                          'total/steps': 0,
-                          'total/hours': 0.}
+        self.combined_stats = {'rollout/return': [],
+                               'eval/return': [],
+                               'total/epochs': [],
+                               'total/episodes': [],
+                               'total/steps': [],
+                               'total/hours': []}
         # Logger
         self.logger = CSVLogger(os.path.join(self.save_path_prefix, 'results.csv'),
-                                keys=combined_stats.keys(), mode='write')
+                                keys=self.combined_stats.keys(), mode='write')
         return
             
     
@@ -217,11 +230,11 @@ class DQN:
                 break
                                                     
             # Select action
-            action = self.get_action(obs, deterministic=False)
+            action = self.get_action(obs, deterministic=False)            
             
             # Render env
-            if self.explore_render and self.epochs_so_far % self.log_interval == 0:
-                self.env.render()
+            # if self.explore_render and self.epochs_so_far % self.log_interval == 0:
+                # self.env.render()
                 
             # Execute action
             new_obs, reward, terminated, truncated, info = self.env.step(action)
@@ -236,6 +249,7 @@ class DQN:
             
             # update exploration rate after each env step
             self.exploration_rate = self.exploration_schedule(self.current_progress_remaining)
+            # print('exploration rate: {}'.format(self.exploration_rate))
             # update the current progress needed for the exploration schedule
             self.current_progress_remaining = 1.0 - float(self.steps_so_far) / float(self.total_timesteps)
 
@@ -244,6 +258,8 @@ class DQN:
             obs = new_obs                        
             
             if done:
+                print('steps so far: {}, episode reward: {}, exploration rate: {}'.format(
+                    self.steps_so_far, self.episode_reward, self.exploration_rate))
                 self.epoch_episode_rewards.append(self.episode_reward)
                 self.episode_reward = 0
                 self.episode_step = 0
@@ -256,16 +272,49 @@ class DQN:
         return is_train_over
         
         
-    def train(self):
-        epoch_actor_losses = []
-        epoch_critic_losses = []
-        epoch_adaptive_distances = []
-        for t_train in range(self.n_train_steps):
-            pass
-        
+    def train(self):                        
+        for _ in range(self.n_train_steps):
+            qloss = self.train_step()
+            self.q_losses.append(qloss)        
+            
+            # update the target network
+            soft_update(target=self.target_q_network,
+                        source=self.q_network,
+                        tau=self.tau)
         
     def train_step(self):
-        self.replay_buffer.sample(batch_size=self.batch_size)
+        batch = self.replay_buffer.sample(self.batch_size)
+        
+        # update critic
+        self.q_network.zero_grad()        
+        qloss = self.q_loss(batch)
+        qloss.backward()
+        self.optimizer.step()
+        return qloss.detach().cpu().data.numpy()
+        
+        
+    def q_loss(self, batch):
+        prev_states = batch['prev_states']
+        next_states = batch['next_states']
+        rewards = batch['rewards']
+        actions = batch['actions']
+        done = batch['done']
+        
+        norm_obs0 = Tensor(normalize(prev_states, self.obs_rms)).to(self.device)
+        norm_obs1 = Tensor(normalize(next_states, self.obs_rms)).to(self.device)
+        rewards = Tensor(rewards).to(self.device)
+        actions = Tensor(actions).to(self.device)
+        done = Tensor(done).to(self.device)
+        
+        predicted_q = self.q_network(norm_obs0) # [bs x actionDim]
+        # predicted_q = predicted_q[actions.to(torch.int32)] # [bs x 1]
+        predicted_q = predicted_q.gather(1, actions.to(torch.int64))
+        target_q = self.target_q_network(norm_obs1)        
+        max_target_q, _ = torch.max(target_q, dim=1) # [bs x 1]
+        max_target_q = max_target_q.reshape(-1, 1)
+        desired_q = rewards + (1. - done) * self.gamma * max_target_q # Only use target_q if obs1 isn't a terminal state
+        desired_q = desired_q.detach()
+        return F.mse_loss(predicted_q, desired_q)
     
     
     def learn(self):        
@@ -279,15 +328,15 @@ class DQN:
         eval_obs = None
         if self.eval_env is not None:
             eval_obs = self.eval_env.reset()[0]
-            if self.eval_render:
-                self.eval_env.render()     # (for pyBullet env.) call before env.reset to show a window of the env.
+            # if self.eval_render:
+            #     self.eval_env.render()     # (for pyBullet env.) call before env.reset to show a window of the env.
             
             
         # Define statistics variables
         # ----------------------------                
         total_hours = 0.0
         eval_episode_reward = 0.0
-        eval_episode_rewards = []  # Each item of this list contains sum of immediate rewards for 1 evaluation episode                        
+        
         self.init_train_variables()        
         
         # The main learning loop. It ends when `self.total_steps >= total_timesteps`
@@ -298,7 +347,23 @@ class DQN:
                 self.epochs_so_far += 1
                 
                 self.collect_rollout_steps()                
-                self.train()                
+                self.train()                                
+            
+            # Log statistics
+            if len(self.epoch_episode_rewards) > 0:
+                self.combined_stats['total/epochs'].append(self.epochs_so_far)
+                self.combined_stats['total/episodes'].append(self.episodes_so_far)
+                self.combined_stats['total/steps'].append(self.steps_so_far)
+                self.combined_stats['rollout/return'].append(np.mean(self.epoch_episode_rewards))            
+
+            if self.eval_env is not None and len(self.eval_episode_rewards) > 0:
+                self.combined_stats['eval/return'].append(np.mean(self.eval_episode_rewards))
+
+            if len(self.combined_stats['rollout/return']) > 0:
+                self.plot_return_vs_step(self.combined_stats)
+                self.print_stats()
+            if self.obs_rms is not None:
+                self.plot_running_avg('obs')
                                 
          
     def get_action(self, obs, deterministic):
@@ -323,8 +388,8 @@ class DQN:
             else:
                 # Greedy action selection
                 q_values = self.q_network(norm_obs)
-                action = q_values.argmax(dim=1).reshape(-1)
-                action = action.cpu().data.numpy()
+                action = q_values.argmax(dim=0).reshape(-1)
+                action = action.cpu().data.numpy().astype(np.int64)[0]
         return action
     
     def store_transition(self, curr_obs, action, reward, next_obs, done):
@@ -342,8 +407,62 @@ class DQN:
         # Running avg/std update
         if self.normalize_obs:
             self.obs_rms.update( np.array([curr_obs]) )
+            self.obs_rms_history['mean'].append(np.mean(self.obs_rms.mean))
+            self.obs_rms_history['std'].append(np.mean(self.obs_rms.std))
         if self.normalize_reward:
             self.reward_rms.update(np.array([reward]))
+            
+            
+    def plot_running_avg(self, running_avg_name='obs'):
+    
+        if running_avg_name == 'obs':
+            m = np.array(self.obs_rms_history['mean'])
+            sig = np.array(self.obs_rms_history['std'])
+        else:
+            m = np.array(self.reward_rms_history['mean'])
+            sig = np.array(self.reward_rms_history['std'])
+        x = np.arange(0, m.size) / 1000.0
+    
+        plt.figure("Running mean/std")
+        plt.title('Running mean/std', fontsize=10)
+        plt.plot(x, m, 'green')
+        plt.fill_between(x, m - sig, m + sig, alpha=0.3, facecolor='green', linewidth=0)
+        plt.xlabel('Update step', fontsize=8)
+        plt.ylabel('mean/std', fontsize=8)
+        plt.savefig(
+            os.path.join(
+                self.save_path_prefix,
+                running_avg_name + '_rms_' + self.env_name + '.png'))
+        plt.close()
+        
+        
+    def plot_return_vs_step(self, combined_stats):
+
+        steps = np.array(combined_stats['total/steps']) / 1000.0
+        returns = combined_stats['rollout/return']
+        eval_returns = combined_stats['eval/return']
+        fig_title = 'episode_reward_vs_Step'
+        plt.figure(fig_title)
+        plt.title('Episode reward vs step')
+        plt.plot(steps, returns, label='rollout ret.')
+        if self.eval_env is not None and len(eval_returns) > 0:
+            plt.plot(steps, eval_returns, label='eval. ret.')
+        plt.xlabel('Steps (thousands)')
+        plt.ylabel('Episode reward average')
+        plt.grid()
+        plt.legend()
+        plt.savefig(
+            os.path.join(self.save_path_prefix,
+                         fig_title + self.env_name + '.png'))
+        plt.close()        
+    
+    def print_stats(self):
+        print('----------------------------------------\n')
+        for key, value in self.combined_stats.items():
+            if type(value) == list and len(value) > 0:
+                print('{0}: {1}'.format(key, value[-1]))
+            else:
+                print('{0}: {1}'.format(key, value))
     
 
 
--- src/algos/dqn/main.py
+++ src/algos/dqn/main.py
@@ -2,9 +2,8 @@ import sys
 sys.path.append('I:/projs/game-ai/src')
 
 from common.parse_args import parse_arguments
-import numpy as np
+import torch      
 import gymnasium as gym
-import torch.nn as nn
 from algos.dqn.dqn import DQN
 
 
@@ -14,7 +13,7 @@ def main():
     args_dict = parse_arguments(sys.argv)
     
     # setup env        
-    env = gym.make(args_dict['env_id'], render_mode="human")
+    env = gym.make(args_dict['env_id'])#, render_mode="human")
     eval_env = gym.make(args_dict['env_id'], render_mode="human")
     
     state_dim = env.observation_space.shape[0]            
@@ -29,8 +28,11 @@ def main():
     for l in args_dict['critic_hidden_layers']:
         hidden_layers.append(int(l))
     
-    # setup DQN agent        
-    activation_ = getattr(nn, args_dict['critic_activation'])
+    # setup DQN agent          
+    if args_dict['critic_activation'] == 'ReLU':
+        activation_ = torch.relu    
+    else:
+        raise NotImplementedError
     dqn = DQN(int(args_dict['n_train_steps']), 
               int(args_dict['n_rollout_steps']),
               int(args_dict['n_eval_steps']),
@@ -47,8 +49,8 @@ def main():
               tau=float(args_dict['tau']),
               lr=float(args_dict['lr']),
               bs=int(args_dict['batch_size']),
-              exploration_fraction=0.1,
-              exploration_initial_eps=1.0, exploration_final_eps=0.05,
+              exploration_fraction=0.12,
+              exploration_initial_eps=1., exploration_final_eps=0.05,
               replay_buff_size=int(float(args_dict['replay_buff_size'])),
               explore_render=bool(args_dict['explore_render']),
               eval_render=bool(args_dict['eval_render']),
