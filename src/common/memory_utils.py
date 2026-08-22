a4752f95951130dbf373b8041df8407920bf5a1b
hhnavid <hhnavid@gmail.com>
2026-08-21 18:42:31 -0700

    src->src-py rename

--- src/common/memory_utils.py
+++
@@ -1,9 +0,0 @@
-import os
-import psutil
-
-
-def mem_usage_in_mb():
-    # return the memory usage in MB
-    process = psutil.Process(os.getpid())
-    mem = process.memory_info()[0] / float(2 ** 20)
-    return mem
--- src/envs/list_gym_envs.py
+++
@@ -1,6 +0,0 @@
-import gymnasium as gym
-
-# list all of the available envs from gymnasium
-for env_id in gym.envs.registry.keys():
-    print(env_id)
-    
